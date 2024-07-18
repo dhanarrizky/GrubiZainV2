b403c14cfa73af79a22b4b47fa053fcb3ad09feb
dhanarrizky <dhanarrizky23@gmail.com>
2024-07-18 20:05:54 +0700

    updateing code for javascript checking nav active

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -1,18 +1,53 @@
 const bars = document.querySelector('#bars');
 const hidebars = document.querySelector('.hide__bars');
+// checking scrolling  position
+const sections = document.querySelectorAll(".sections");
+const navLinks = document.querySelectorAll(".navbar_link--links a");
+// adding and removeing class active on hidden bar
+const navHiddenLinks = document.querySelectorAll('.hide__bars__links');
+
 
 const methodAddRemoveActiveClass = () => {
     hidebars.classList.toggle('active');
 }
 
+
 bars.addEventListener('click', () => {
     methodAddRemoveActiveClass();
 });
 
-// Menambahkan dan menghapus kelas aktif pada daftar bar tersembunyi
-const navHiddenLinks = document.querySelectorAll('.hide__bars__links');
+
 navHiddenLinks.forEach(btn => {
     btn.addEventListener('click', () => {
         methodAddRemoveActiveClass();
     });
 });
+
+
+window.addEventListener('scroll', () => {
+    let current = '';
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (pageYOffset >= sectionTop - sectionHeight / 3) {
+            current = section.getAttribute('id');
+        }
+    });
+    
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href') === '#' + current) {
+            link.classList.add('active');
+        }
+    });
+})
+
+
+function checkWidth() {
+    if (window.innerWidth >= 790) {
+        hidebars.classList.remove('active');
+    }
+}
+
+window.addEventListener('resize', checkWidth);
+checkWidth();
